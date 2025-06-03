3d8f8e9adb03635f572e526c4bdcbd7625fe39df
DinithiLiyanage <dinithi.adithya@gmail.com>
2025-06-03 22:23:29 +0530

    Completed upto about me

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,111 @@
 import Image from "next/image";
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="min-h-screen flex  flex-col items-center bg-gradient-to-br from-[#0f2027] via-[#2c5364] to-[#1F2428] relative">
+      {/* Content Container */}
+      <div className="w-full min-h-screen relative z-10 flex justify-between items-center flex-col lg:flex-row py-20">
+        {/* Animated Gradient Background */}
+        <div
+          className="absolute inset-0 -z-10 bg-gradient-to-br from-[#0B0C10] via-[#1F2833] via-[#C5C6C7] via-[#66FCF1] to-[#45A29E] bg-[length:400%_400%] opacity-40"
+          style={{
+            animation: "gradientBG 8s ease infinite",
+          }}
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        {/* Left Content */}
+        <div className="text-white space-y-4 lg:w-1/2 px-20">
+          <h2 className="text-xl font-medium">Hi, I am</h2>
+          <h1 className="text-4xl sm:text-6xl font-bold text-white">
+            Dinithi Liyanage
+          </h1>
+          <p className="text-[#66FCF1] text-lg">
+            FULL-STACK DEVELOPER | CYBERSECURITY ENTHUSIAST
+          </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="flex gap-4 pt-4">
+            <a
+              href="dinithi.adithya@gmail.com"
+              className="bg-gray-600 p-3 rounded shadow hover:bg-gray-400"
+            >
+              <FaEnvelope />
+            </a>
+            <a
+              href="https://github.com/DinithiLiyanage"
+              target="_blank"
+              className="bg-gray-600 p-3 rounded shadow hover:bg-gray-400"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://linkedin.com/in/dad-liyanage/"
+              target="_blank"
+              className="bg-gray-600 p-3 rounded shadow hover:bg-gray-400"
+            >
+              <FaLinkedin />
+            </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        {/* Right Profile Image */}
+        <div className="lg:w-1/2 flex justify-left pt-1 pr-30 lg:pt-0">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/Dinithi.JPG" // Transparent PNG
+            alt="Profile"
+            width={500}
+            height={500}
+            className="object-cover rounded-lg shadow-lg"
+            priority
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
+
+      {/* Navigation */}
+      {/* <div className="absolute z-20 top-6 right-10 flex justify-left gap-20 text-white text-sm">
+        <a href="#about">About me</a>
+        <a href="#skills">Skills</a>
+        <a href="#portfolio">Portfolio</a>
+        <button className="bg-white text-black px-4 py-1 rounded-full font-medium shadow hover:scale-105 transition">
+          Contact Me
+        </button>
+      </div> */}
+
+      <div className="w-full flex justify-between items-center flex-col py-20 px-10">
+        <h2 className="text-4xl font-medium text-white">About Me</h2>
+        <p className="text-white text-center text-xl py-10 leading-relaxed px-20">
+          I'm Dinithi Liyanage, a Computer Science and Engineering undergraduate
+          at the University of Moratuwa, specializing in cybersecurity. As a
+          passionate full-stack developer, I enjoy building web applications and
+          exploring emerging technologies. My expertise includes Node.js, Java,
+          and React, and I thrive on solving real-world problems through
+          software development. I am highly motivated, analytical, and enjoy
+          collaborating in dynamic teams.
+          <br />
+        </p>
+      </div>
+
+      <div className="w-full flex flex-col items-center py-20 px-10">
+        <h2 className="text-4xl font-medium text-white">Education</h2>
+        <div className="flex flex-row justify-center items-stretch gap-8 w-full py-10">
+          <div className="flex-1 bg-[rgba(197,198,199,0.4)] rounded-lg shadow-lg p-6">
+            <p className="text-white text-center text-xl leading-relaxed px-6">
+              BSc in Computer Science and Engineering (Special) <br />
+              University of Moratuwa, Sri Lanka <br />
+              (2022 - Present)
+            </p>
+          </div>
+          <div className="flex-1 bg-[rgba(197,198,199,0.4)] rounded-lg shadow-lg p-6">
+            <p className="text-white text-center text-xl leading-relaxed px-6">
+              G.C.E. Advanced Level
+            </p>
+          </div>
+          <div className="flex-1 bg-[rgba(197,198,199,0.4)] rounded-lg shadow-lg p-6">
+            <p className="text-white text-center text-xl leading-relaxed px-6">
+              G.C.E. Ordinary Level
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
